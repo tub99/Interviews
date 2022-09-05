a974e545c2751cb56cddacb9dba5ba89bf32defd
Soumya Ghosh <ghoshsoumya001@gmail.com>
2022-09-05 15:44:43 +0530

    dsa

---
+++ SystemDesign/LLD/DSA/Strings/LongestUniqueSubString.java
@@ -0,0 +1,9 @@
+/**
+    Given a string str, find the length of the longest substring without repeating characters. 
+    For “GEEKSFORGEEKS”, there are two longest substrings shown in the below diagrams, with length 7
+    Output: EKSFORG, KSFORGE
+
+    Ex 2:
+    Input: clementisacap
+    Output: mentisac
+ */
